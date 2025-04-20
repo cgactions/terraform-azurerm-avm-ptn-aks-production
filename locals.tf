7e26cecfaff644601331120da135ce57a6e397bb
christophergunn <christopherjamesgunn@icloud.com>
2025-04-20 21:51:44 +0100

    locals

--- locals.tf
+++ locals.tf
@@ -4,38 +4,44 @@ locals {
     "private\\.[a-z0-9]+\\.azmk8s\\.io",
     "privatelink\\.[a-z0-9]+\\.azmk8s\\.io",
     "[a-zA-Z0-9\\-]{1,32}\\.private\\.[a-z]+\\.azmk8s\\.io",
-    "[a-zA-Z0-9\\-]{1,32}\\.privatelink\\.[a-z]+\\.azmk8s\\.io",
+    "[a-zA-Z0-9\\-]{1,32}\\.privatelink\\.[a-z]+\\.*azmk8s\\.io",
   ]
 }
 
+# Only try to read VM SKUs if the data is available
 locals {
-  default_node_pool_available_zones = setsubtract(local.zones, local.restricted_zones)
-  filtered_vms = [
+  filtered_vms = can(data.azapi_resource_list.example.output.value) ? [
     for sku in data.azapi_resource_list.example.output.value :
     sku if(sku.resourceType == "virtualMachines" && sku.name == var.default_node_pool_vm_sku)
-  ]
+  ] : []
+
   restricted_zones = try(local.filtered_vms[0].restrictions[0].restrictionInfo.zones, [])
-  zones            = local.filtered_vms[0].locationInfo[0].zones
+  zones            = try(local.filtered_vms[0].locationInfo[0].zones, [])
+  default_node_pool_available_zones = setsubtract(local.zones, local.restricted_zones)
 }
 
 locals {
-  filtered_vms_by_node_pool = {
+  filtered_vms_by_node_pool = var.node_pools != null && can(data.azapi_resource_list.example.output.value) ? {
     for pool_name, pool in var.node_pools : pool_name => [
       for sku in data.azapi_resource_list.example.output.value :
       sku if(sku.resourceType == "virtualMachines" && sku.name == pool.vm_size)
     ]
-  }
-  my_node_pool_zones_by_pool = {
+  } : {}
+
+  my_node_pool_zones_by_pool = var.node_pools != null && can(local.filtered_vms_by_node_pool) ? {
     for pool_name, pool in var.node_pools : pool_name => setsubtract(
-      local.filtered_vms_by_node_pool[pool_name][0].locationInfo[0].zones,
+      try(local.filtered_vms_by_node_pool[pool_name][0].locationInfo[0].zones, []),
       try(local.filtered_vms_by_node_pool[pool_name][0].restrictions[0].restrictionInfo.zones, [])
     )
-  }
-  zonetagged_node_pools = {
-    for pool_name, pool in var.node_pools : pool_name => merge(pool, { zones = local.my_node_pool_zones_by_pool[pool_name] })
-  }
-}
+  } : {}
 
+  zonetagged_node_pools = var.node_pools != null && can(local.my_node_pool_zones_by_pool) ? [
+    for pool_name, pool in var.node_pools : merge(pool, {
+      zones = try(local.my_node_pool_zones_by_pool[pool_name], [])
+      name  = pool_name
+    })
+  ] : []
+}
 
 locals {
   node_pools = var.node_pools != null && can(length(keys(var.node_pools))) ? flatten([
@@ -61,9 +67,6 @@ locals {
   log_analytics_tables = ["AKSAudit", "AKSAuditAdmin", "AKSControlPlane", "ContainerLogV2"]
 }
 
-# Helper locals to make the dynamic block more readable
-# There are three attributes here to cater for resources that
-# support both user and system MIs, only system MIs, and only user MIs
 locals {
   managed_identities = {
     user_assigned = length(var.managed_identities.user_assigned_resource_ids) > 0 ? {
@@ -71,7 +74,7 @@ locals {
         type                       = "UserAssigned"
         user_assigned_resource_ids = var.managed_identities.user_assigned_resource_ids
       }
-      } : {
+    } : {
       this = {
         type                       = "UserAssigned"
         user_assigned_resource_ids = azurerm_user_assigned_identity.aks[*].id
